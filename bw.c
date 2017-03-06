bc29dd1f8ab3a9080725869d6a03ef0d987a2d4d
orebuc <pierre.cuberocastan@gmail.com>
2017-03-06 11:59:15 +0100

    Amélioration du rendu résultat de l'encrypt + amélioration de l'encrypt (maintenant par block)

--- bw.c
+++ bw.c
@@ -7,7 +7,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct{
+
+#define BLOCK_SIZE 42
+#define BUFFER_SIZE 10
+
+
+
+typedef struct {
   char *s;
   int index;
 }encrypted_string;
@@ -16,18 +22,16 @@ typedef struct{
 int compare (const void *a, const void *b) {
     const char **ia = (const char **)a;
     const char **ib = (const char **)b;
-    return strcmp(*ia, *ib);
+    return strcmp (*ia, *ib);
 }
 
 encrypted_string sort (char **s, unsigned len) {
   char *word = malloc (len * sizeof (char));
   strcpy (word, s[0]);
-  qsort(s, len, sizeof(char *), compare);
+  qsort (s, len, sizeof (char *), compare);
   int index = 0;
   unsigned i;
   for (i = 0; i < len; i++)
-    printf("%s\n", s[i]);
-  for (i = 0; i < len; i++) 
     if (!strcmp (word, s[i]))
       break;
   index = i;
@@ -37,27 +41,27 @@ encrypted_string sort (char **s, unsigned len) {
 }
 
 char *shift (int n, char *s) {
-  char *a = malloc (strlen (s) *sizeof (char));
+  char *a = malloc (BLOCK_SIZE *sizeof (char));
   unsigned i;
-  for (i = 0; i < strlen (s); i++) {
+  for (i = 0; i < BLOCK_SIZE; i++) {
     int pos = i+n;
-    if (i + n >= strlen(s))
-      pos -= strlen(s);
+    if (i + n >= BLOCK_SIZE)
+      pos -= BLOCK_SIZE;
     a[pos] = s[i];
   }
   return a;
 }
 
 
-encrypted_string encrypt (char *s) {
-  char **strings = malloc (sizeof (char *) * strlen (s));
-  unsigned i, len = strlen (s);
-  
+encrypted_string encrypt (char *s, unsigned len) {
+  char **strings = malloc (sizeof (char *) * BLOCK_SIZE);
+  unsigned i;
+
   /* Shift */
   for (i = 0; i < len; i++) {
-    strings[i] = shift(i, s);
+    strings[i] = shift (i, s);
   }
-  
+
   /* Sort */
   encrypted_string e = sort (strings, len);
 
@@ -69,13 +73,11 @@ encrypted_string encrypt (char *s) {
 
 
 void
-delta_encode (char *buffer, const unsigned int length)
-{
+delta_encode (char *buffer, const unsigned int length) {
  char delta = 0;
  char original;
  unsigned int i;
- for (i = 0; i < length; ++i)
- {
+ for (i = 0; i < length; ++i) {
    original = buffer[i];
    buffer[i] -= delta;
    delta = original;
@@ -84,19 +86,39 @@ delta_encode (char *buffer, const unsigned int length)
 
 int main (int argc, char **argv) {
   if (argc < 2)
-    exit(EXIT_FAILURE);
-  
-  encrypted_string es = encrypt (argv[1]);
-  printf ("Encrypted Message : %d:%s\n", es.index, es.s);
+    exit (EXIT_FAILURE);
+  char *arg = argv[1];
+  char *block = malloc (strlen(argv[1]) * 2);
 
-  delta_encode(es.s, strlen(es.s));
+  char buffer[BUFFER_SIZE];
+  unsigned i;
+  encrypted_string es;
+  for (i = 0; i < (strlen (argv[1]) / BLOCK_SIZE); i++) {
+    es = encrypt (arg, BLOCK_SIZE);
+    sprintf (buffer, "%d", es.index);
+    buffer[BUFFER_SIZE-1] = '\0';
+    strcat (block, buffer);
+    strcat (block, ":");
+    strcat (block, es.s);
+    strcat (block, "\n");
+    arg += BLOCK_SIZE;
+  }
+
+  /* Last char to encrypt */
+  es = encrypt (arg, strlen(arg));
+  sprintf (buffer, "%d", es.index);
+  buffer[BUFFER_SIZE-1] = '\0';
+  strcat (block, buffer);
+  strcat (block, ":");
+  strcat (block, es.s);
+  strcat (block, "\n");
+  strcat (block, "\0");
+  printf ("%s\n", block);
+
+  //  printf ("Encrypted Message : %d:%s\n", es.index, es.s);
+
+//  delta_encode (es.s, strlen (es.s));
   free (es.s);
+  free (block);
   return EXIT_SUCCESS;
 }
-
-
-/* 
-#define TAILLE_COUPE 40
-
-
- */
