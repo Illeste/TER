4cac352c7a49e382a7c11c68d8584f18284c7f57
HalfSky <b.maxime336@gmail.com>
2017-03-13 13:16:48 +0100

    Sort ne marche pas, a corriger

--- bw.c
+++ bw.c
@@ -14,69 +14,86 @@
 #include <stdint.h>
 
 /* Size of each block passed through Burrows Wheeler */
-#define BLOCK_SIZE 40
+#define BLOCK_SIZE 7
 #define BUFFER_SIZE 10
 /* Alphabet used in Huffman's Algorithm has for size 2**LETTER_SIZE */
 #define LETTER_SIZE 8 
 
+void print_array (uint8_t a) {
+  int i;
+  for (i = 7; i >= 0; i--)
+    (a & (1 << i)) == 0 ?printf("0"): printf("1");
+  printf(" ");
+}
+
+
 int compare (const void *a, const void *b) {
   uint8_t *_a = (uint8_t *) a;
   uint8_t *_b = (uint8_t *) b;
-  unsigned i, j;
+  int i;
+  for (i = 0; i < BLOCK_SIZE; i++)
+    print_array (_a[i]);
+  printf("\n");
+  for (i = 0; i < BLOCK_SIZE; i++)
+    print_array (_b[i]);
+  printf("\n");
+
+
   /* Little endian storage */
-  for (j = 0; j < BLOCK_SIZE; j++) {
-    for (i = LETTER_SIZE - 1; i != 0; i--) {
-      if ((_a[j] & (1 << i)) != (_b[j] & (1 << i)))
-	return (_a[j] & (1 << i)) - (_b[j] & (1 << i));
+  for (i = 0; i < BLOCK_SIZE; i++) {
+    if (_a[i] != _b[i]) {
+      printf("%d\n\n", _a[i] - _b[i]);
+      return _a[i] - _b[i];
     }
   }
+  printf("0\n\n");
   return 0;
 }
 
-/* Sorts to_sort array, and writes index then last letter of each sorted word */
-void sort (uint8_t **to_sort, uint8_t *s) {
-  qsort (to_sort, BLOCK_SIZE, sizeof (uint8_t), compare);
-  unsigned i;
-  printf("ok\n");
-  /* for (i = 0; i < BLOCK_SIZE; i++) */
-  /*   if (*to_sort[i] == *s) */
-  /*     break; */
-  /* printf ("ok\n"); */
-  /* uint8_t index = i; */
-  /* s[0] = (uint8_t) index; */
-  /* for (i = 0; i < BLOCK_SIZE; i++) */
-  /*   s[index + 1] = to_sort[i][BLOCK_SIZE + 1]; */
-}
-
-/* Returns the input shifted one to the right, 
- * leaving the first block for the future index */
-uint8_t *shift (int n, uint8_t *s) {
-  uint8_t *a = malloc (BLOCK_SIZE *sizeof (uint8_t));
-  unsigned i;
-  for (i = 1; i <= BLOCK_SIZE; i++) {
-    int pos = i + n;
-    if (i + n > BLOCK_SIZE)
-      pos -= BLOCK_SIZE;
-    a[pos] = s[i];
+void sort(uint8_t *to_sort[], uint8_t s[]) {
+  /* Sort the array by lexicographical order */
+  qsort (to_sort, BLOCK_SIZE, sizeof (uint8_t *), compare);
+  unsigned index = 65;
+  int i, j;
+  for (i = 0; i < BLOCK_SIZE; i++) {
+    for (j = 0; j < BLOCK_SIZE; j++)
+      print_array (to_sort[i][j]);
+    printf("\n");
   }
-  return a;
+  /* Search pos of the original string */
+  s[0] = index;
 }
 
 
-void print_array (uint8_t a) {
-  unsigned i;
-  for (i = 0; i < 8; i++)
-    (a & (1 << i)) == 0 ?printf("0"): printf("1");
-  printf("\n");
+uint8_t *shift (uint8_t *s) {
+  uint8_t *shifted_s = malloc (BLOCK_SIZE * sizeof(uint8_t)); 
+  int i;
+  for (i = 0; i < BLOCK_SIZE; i++) {
+    int pos = i + 1;
+    if (pos == BLOCK_SIZE)
+      pos = 0;
+    shifted_s[pos] = s[i];
+  }
+  return shifted_s;
 }
 
 
 void burrows_wheeler (uint8_t *s) {
-  uint8_t **strings = malloc (sizeof (uint8_t *) * BLOCK_SIZE);
-  unsigned i;
-  for (i = 0; i < BLOCK_SIZE; i++)
-    strings[i] = shift (i, s);
+  uint8_t **strings = malloc (sizeof (uint8_t *) * (BLOCK_SIZE));
+  unsigned i, j;
+  strings[0] = s + 1 ;
+  for (i = 1; i < BLOCK_SIZE; i++)
+    strings[i] = shift (strings[i-1]);
+  for (i = 0; i < BLOCK_SIZE; i++) {
+    for (j = 0; j < BLOCK_SIZE; j++) {
+      print_array (strings[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
   sort (strings, s);
+  /* for (i = 0; i < BLOCK_SIZE; i++) */
+  /*   free(strings[i]); */
   free (strings);
 }
 
