97d23af756d1071773a90983adb519dbcd9c6d21
HalfSky <b.maxime336@gmail.com>
2017-04-04 15:01:15 +0200

    Ajout fichier decompression2

--- u_bw.c
+++ u_bw.c
@@ -14,11 +14,91 @@
 #include <sys/stat.h>
 #include <stdint.h>
 
+/* Size of each block passed through Burrows Wheeler */
+/* !!!!! Faire gaffe si on depasse la taille d'un uint (8 ou 16) pour index */
+#define BLOCK_SIZE 20
+/* Size of data scanned (2 bytes because we use uint16_t) */
+#define DATA_SIZE 2
+/* Alphabet used in Huffman's Algorithm has for size 2**LETTER_SIZE */
+#define LETTER_SIZE 16
+#define BYTES_SIZE 8
+/* Should be 2**LETTER_SIZE */
+#define ALPHABET_SIZE 256
+/* Amount of data scanned */
+#define DATA_READ 100000
+/* Name of in betwwen files for storage */
+#define RETURN_BW           "result_bw"
+#define INDEX_BW            ".index_bw"
+#define RETURN_ENC          "result_encode"
+#define DICTIONNARY_ENC     ".dico_enc"
+#define RETURN_HUF          "result_huffman"
+#define ENCODE_HUF          ".code_huff"
+
+
+/* Huffman Structs */
+typedef struct node {
+  uint8_t data;
+  unsigned amount;
+  struct node *left, *right;
+} node_t;
+
+typedef struct {
+  unsigned depth;
+  uint8_t *encoding;
+}transform_t;
+
+/* Encoding Structs */
+struct list {
+  uint8_t data;
+  struct list *next;
+};
+
+typedef struct list *list;
+
+
+void print_array (uint16_t a) {
+  int i;
+  for (i = LETTER_SIZE - 1; i >= 0; i--)
+    (a & (1 << i)) == 0 ?printf("0"): printf("1");
+  printf(" ");
+}
+
+void print_encoding (transform_t t) {
+  unsigned i, j;
+  for (i = 0; i < (t.depth + 1) / 8; i++) {
+    for (j = 8; j != 0; j--)
+      (t.encoding[i] & (1 << (j - 1))) == 0 ?printf("0"): printf("1");
+  }
+  if ((t.depth + 1)  % 8) {
+    for (j = t.depth % 8; j != 0; j--)
+      (t.encoding[i] & (1 << (j-1))) == 0 ?printf("0"): printf("1");
+  }
+}
+
+void print_sort_tab (uint8_t **tab, unsigned block_size) {
+  unsigned i, j;
+  for (i = 0; i < block_size; i++) {
+    for (j = 0; j < block_size; j++) {
+      printf("%c", tab[i][j]);
+//      print_array (tab[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+
 /* DECOMPRESSION BW */
+
+
 
 /* DECOMPRESSION MTF */
 
+
+
 /* DECOMPRESSSION HUFFMAN */
+
+
 
 int main () {
   /* Ouverture fichier compressé
