3851607d84d212b793e14246d207eff633578cbb
HalfSky <b.maxime336@gmail.com>
2017-04-04 14:57:06 +0200

    Ajout fichier decompression

---
+++ u_bw.c
@@ -0,0 +1,32 @@
+#define _GNU_SOURCE
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <ctype.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+/* DECOMPRESSION BW */
+
+/* DECOMPRESSION MTF */
+
+/* DECOMPRESSSION HUFFMAN */
+
+int main () {
+  /* Ouverture fichier compressé
+   * + Huffman a l'eners : récupère le fichier post mft
+   * donc mtf dans le meme sens, copier coller fonction
+   * bw decompression
+   *
+   * Retourner, ecrire dans meme fichier/ailleurs decompression 
+   */
+  return EXIT_SUCCESS;
+}
